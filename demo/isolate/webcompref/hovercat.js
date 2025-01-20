ff1dcb593e531d3b3c9399bffe2fe9786823a23e
Lawrence Joel <ljoel@outlook.com>
2025-01-20 15:05:11 +0000

    not need the domcontentload

--- demo/isolate/webcompref/hovercat.js
+++ demo/isolate/webcompref/hovercat.js
@@ -2,19 +2,17 @@ var HoverCat;
 (function (HoverCat) {
     console.log("applied hovercat");
 
-    function init() {
-        document.addEventListener('DOMContentLoaded', function () {
-            const thumbnail = document.querySelector('.thumbnail');
-            const largeImage = document.querySelector('.large-image');
+    function init() {        
+        const thumbnail = document.querySelector('.thumbnail');
+        const largeImage = document.querySelector('.large-image');
 
-            thumbnail.addEventListener('mouseover', function () {
-                largeImage.style.display = 'block';
-            });
-
-            thumbnail.addEventListener('mouseout', function () {
-                largeImage.style.display = 'none';
-            });
+        thumbnail.addEventListener('mouseover', function () {
+            largeImage.style.display = 'block';
         });
+
+        thumbnail.addEventListener('mouseout', function () {
+            largeImage.style.display = 'none';
+        });        
     }
 
     HoverCat.init = init;
